ab6a306fd72d98f320e0a24b96f9d4e3438a87d3
vishalbhangare <vishalbhangare2@gmail.com>
2023-07-07 19:43:59 +0530

    styles for tooltip done

--- src/components/Home/Header/Header.tsx
+++ src/components/Home/Header/Header.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import styles from "./Header.module.scss";
 const Header = () => {
-  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isLoggedIn, setLoggedIn] = useState(true);
 
   return (
     <div className={styles.container}>
       <span className={styles.logo}>Morningstall</span>{" "}
       {/* <img src="src/assets/logo.png" alt="loading" /> */}
-      <button>Signin</button>
-      {isLoggedIn && <i className="fa-duotone fa-books"></i>}
+      <button
+        className={styles.tooltip_bottom}
+        data-tooltip-content="Click here to proceed"
+      >
+        Signin
+      </button>
       {isLoggedIn && <i className="fa-duotone fa-user"></i>}
+      {isLoggedIn && (
+        <i
+          className={["fa-duotone", "fa-books", styles.bookshelfIcon].join(" ")}
+        ></i>
+      )}
     </div>
   );
 };
